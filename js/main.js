a15dfd92b52cd999d4f1ddbc1bdf7d4c5fb273e7
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-15 14:59:19 +0100

    esercizio senza bonus

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 const {createApp}= Vue;
 createApp({
     data(){
+        
         return{
-            message : 'hello world'
+            toAdd:'',
+            Tasks:[],   
+        }
+    },
+    methods:{
+        addTask(){
+            const newTask={
+                task : this.toAdd,
+                isDone : false,
+            }
+            this.Tasks.push(newTask);
+        },
+        removeTask(i){
+            this.Tasks.splice(i,1);
+        },
+        markDone(i){
+            console.log(this.Task[i].task)
+            //if(this.Tasks[i].isDone== false){
+              //  alert('haifhie')
+            //}
         }
     }
 }).mount('#app');
